d7c2fce8f7a79e94d427b6ea3641b36fabaa3af8
slayerfat <slayerfat@gmail.com>
2015-01-21 14:41:44 -0430

    terminado el tutorial

--- app/libraries/Calculator.php
+++ app/libraries/Calculator.php
@@ -4,6 +4,10 @@ namespace App\libraries;
 class Calculator {
 
   public function Add($x, $y){
+
+    if ( !is_numeric($x) or !is_numeric($y) ) :
+      throw new \InvalidArgumentException;
+    endif;
     return $x + $y;
   }
 
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,14 +1,36 @@
 <?php
 
-require 'vendor/autoload.php';
+use App\libraries\Calculator;
 
 class CalculatorTest extends PHPUnit_Framework_TestCase{
 
-  public function testAddNumbers(){
+  public function setUp(){
+    $this->calculator = new Calculator;
+  }
 
-    $calc = new Calculator;
+  public function inputNumbers(){
+    return [
+      [2, 2, 4],
+      [2.5, 2.5, 5],
+      [-3, 1, -2],
+      [-9, -9, -18]
+    ];
+  }
 
-    $this->assertEquals(4, $calc->add(2,2));
+  /**
+   * @dataProvider inputNumbers
+   */
+  public function testAddNumbers($x, $y, $sum){
+
+    $this->assertEquals($sum, $this->calculator->Add($x, $y));
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testThrowsExceptionIfNonNumericIsPassed(){
+    
+    $this->calculator->Add( 'a', [] );
   }
 
 }
