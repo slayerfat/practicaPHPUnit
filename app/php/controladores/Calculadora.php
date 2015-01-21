ac71d11b0780ed702e20ceb703a94803b551c277
slayerfat <slayerfat@gmail.com>
2015-01-21 15:41:22 -0430

    terminadas test ultra basicos de calculadora

--- app/php/controladores/Calculadora.php
+++ app/php/controladores/Calculadora.php
@@ -7,10 +7,35 @@ class Calculadora extends Controlador{
   public function sumar($x, $y){
 
     if ( !is_numeric($x) or !is_numeric($y) ):   
-      throw new \Exception("Error Processing Request", 1);
+      throw new \Exception("Error, solo se espera numeros", 1);
     endif;
 
     return $x + $y;
+  }
+
+  public function restar($x, $y){
+
+    if ( !is_numeric($x) or !is_numeric($y) ):   
+      throw new \Exception("Error, solo se espera numeros", 1);
+    endif;
+
+    return $x - $y;
+  }
+  public function multiplicar($x, $y){
+
+    if ( !is_numeric($x) or !is_numeric($y) ):   
+      throw new \Exception("Error, solo se espera numeros", 1);
+    endif;
+
+    return $x * $y;
+  }
+  public function dividir($x, $y){
+
+    if ( !is_numeric($x) or !is_numeric($y) ):   
+      throw new \Exception("Error, solo se espera numeros", 1);
+    endif;
+
+    return $x / $y;
   }
   
 }
--- tests/php/controladores/CalculadoraTest.php
+++ tests/php/controladores/CalculadoraTest.php
@@ -7,12 +7,36 @@ class CalculadoraTest extends PHPUnit_Framework_TestCase{
     $this->calculadora = new Calculadora;
   }
 
-  public function numeros(){
+  public function numerosSuma(){
     return [
       [2, 2, 4],
       [2.5, 2.5, 5],
       [-3, 1, -2],
       [-9, -9, -18]
+    ];
+  }
+  public function numerosResta(){
+    return [
+      [2, 2, 0],
+      [2.5, 2.5, 0],
+      [-3, 1, -4],
+      [-9, -9, 0]
+    ];
+  }
+  public function numerosMultiplicion(){
+    return [
+      [2, 2, 4],
+      [2.5, 2.5, 6.25],
+      [-3, 1, -3],
+      [-9, -9, 81]
+    ];
+  }
+  public function numerosDivision(){
+    return [
+      [2, 2, 1],
+      [2.5, 2.5, 1],
+      [-3, 1, -3],
+      [-9, -9, 1]
     ];
   }
   public function noNumeros(){
@@ -24,7 +48,7 @@ class CalculadoraTest extends PHPUnit_Framework_TestCase{
   }
 
   /**
-   * @dataProvider numeros
+   * @dataProvider numerosSuma
    */
   public function testSumarNumeros($x, $y, $resultadoEsperado){
 
@@ -32,11 +56,54 @@ class CalculadoraTest extends PHPUnit_Framework_TestCase{
   }
 
   /**
+   * @dataProvider numerosResta
+   */
+  public function testRestarNumeros($x, $y, $resultadoEsperado){
+
+    $this->assertEquals($resultadoEsperado, $this->calculadora->restar($x, $y));
+  }
+
+  /**
    * @dataProvider noNumeros
    * @expectedException        Exception
-   * @expectedExceptionMessage Error Processing Request
+   * @expectedExceptionMessage Error, solo se espera numeros
    */
-  public function testSoloOperacionConNumeros($x, $y){
+  public function testSumarSoloOperacionConNumeros($x, $y){
+
     $this->calculadora->sumar($x, $y);
+    
+  }
+  /**
+   * @dataProvider noNumeros
+   * @expectedException        Exception
+   * @expectedExceptionMessage Error, solo se espera numeros
+   */
+  public function testRestarSoloOperacionConNumeros($x, $y){
+
+    $this->calculadora->restar($x, $y);
+    
+  }
+  /**
+   * @dataProvider noNumeros
+   * @expectedException        Exception
+   * @expectedExceptionMessage Error, solo se espera numeros
+   */
+  public function testMultiplicarSoloOperacionConNumeros($x, $y){
+
+    $this->calculadora->multiplicar($x, $y);
+    
+  }
+  /**
+   * @dataProvider noNumeros
+   * @expectedException        Exception
+   * @expectedExceptionMessage Error, solo se espera numeros
+   */
+  public function testDividirSoloOperacionConNumeros($x, $y){
+
+    $this->calculadora->sumar($x, $y);
+    
   }
 }
+// $this->calculadora->restar($x, $y);
+// $this->calculadora->multiplicar($x, $y);
+// $this->calculadora->dividir($x, $y);
