bbd8e9a6df69d00302b2133f87c53369d38f5236
slayerfat <slayerfat@gmail.com>
2015-01-22 15:08:20 -0430

    idea basica formulario completo

--- app/php/vistas/calculadora/index.php
+++ app/php/vistas/calculadora/index.php
@@ -14,6 +14,32 @@
     <p>
       <?php echo $datos['infoSistema']['description'] ?>
     </p>
+  </div>
+  <div>
+    <form>
+      <div>
+        <label>
+          Operando 1
+          <input type="number" placeholder="introduzca numero">
+        </label>
+        <label>
+          Operando 2
+          <input type="number" placeholder="introduzca numero">
+        </label>
+      </div>
+      <div>
+        <label>Operacion</label>
+        <input type="radio" name="operacion" value="sumar" id="sumar"><label for="sumar">Sumar</label>
+        <input type="radio" name="operacion" value="restar" id="restar"><label for="restar">Restar</label>
+        <input type="radio" name="operacion" value="multiplicar" id="multiplicar"><label for="multiplicar">Multiplicar</label>
+        <input type="radio" name="operacion" value="dividir" id="dividir"><label for="dividir">Dividir</label>
+      </div>
+      <div>
+        <input type="submit" value="Procesar">
+      </div>
+    </form>
+  </div>
+  <div>
     <p>
       <code><?php var_dump($datos) ?></code>
     </p>
